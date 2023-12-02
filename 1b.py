61af474564d8638cce64051869cea67e7bab54d4
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-12-01 16:51:13 -0800

    1B

---
+++ 1b.py
@@ -0,0 +1,23 @@
+import re
+
+translate = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+su = 0
+with open('1.in') as f:
+    b = f.read()
+    b = re.sub(r'^.*?(one|two|three|four|five|six|seven|eight|nine|\d)', r'\1 ', b, flags=re.M)
+    b = re.sub(r' .*(one|two|three|four|five|six|seven|eight|nine|\d).*?$', r'\1', b, flags=re.M)
+    # handle duplicates
+    b = re.sub(r'^(.*) .*$', r'\1\1', b, flags=re.M)
+    for k, v in translate.items():
+        b = b.replace(k, str(v))
+    print(sum(map(int, b.splitlines())))
